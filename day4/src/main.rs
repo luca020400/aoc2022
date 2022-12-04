7670c9d3d7f8ec789684360648abc20409a0c305
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-12-04 18:29:36 +0100

    day4: propagate errors to main

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -16,34 +16,56 @@ impl<T: std::cmp::PartialOrd> Overlap for RangeInclusive<T> {
 fn main() -> Result<(), Box<dyn Error>> {
     let part_one: u32 = fs::read_to_string("input.txt")?
         .lines()
-        .map(|line| {
-            let (first, second) = line.split_once(',').unwrap();
-            let split_into_range = |str: &str| -> RangeInclusive<_> {
-                let (left, right) = str.split_once('-').unwrap();
-                left.parse::<u32>().unwrap()..=right.parse::<u32>().unwrap()
+        .map(|line| -> Result<u32, Box<dyn Error>> {
+            let (first, second) = match line.split_once(',') {
+                Some(x) => Ok(x),
+                None => Err("Can't split the string"),
+            }?;
+
+            let split_into_range = |str: &str| -> Result<RangeInclusive<u32>, Box<dyn Error>> {
+                let (left, right) = match str.split_once('-') {
+                    Some(x) => Ok(x),
+                    None => Err("Can't split the string"),
+                }?;
+                let left = left.parse::<u32>()?;
+                let right = right.parse::<u32>()?;
+                Ok(left..=right)
             };
-            let (first, second) = (split_into_range(first), split_into_range(second));
-            u32::from(
+            let first = split_into_range(first)?;
+            let second = split_into_range(second)?;
+
+            Ok(u32::from(
                 first.contains(second.start()) && first.contains(second.end())
                     || second.contains(first.start()) && second.contains(first.end()),
-            )
+            ))
         })
-        .sum();
+        .sum::<Result<u32, _>>()?;
 
     println!("Part one: {part_one}");
 
     let part_two: u32 = fs::read_to_string("input.txt")?
         .lines()
-        .map(|line| {
-            let (first, second) = line.split_once(',').unwrap();
-            let split_into_range = |str: &str| -> RangeInclusive<_> {
-                let (left, right) = str.split_once('-').unwrap();
-                left.parse::<u32>().unwrap()..=right.parse::<u32>().unwrap()
+        .map(|line| -> Result<u32, Box<dyn Error>> {
+            let (first, second) = match line.split_once(',') {
+                Some(x) => Ok(x),
+                None => Err("Can't split the string"),
+            }?;
+
+            let split_into_range = |str: &str| -> Result<RangeInclusive<u32>, Box<dyn Error>> {
+                let (left, right) = match str.split_once('-') {
+                    Some(x) => Ok(x),
+                    None => Err("Can't split the string"),
+                }?;
+                let left = left.parse::<u32>()?;
+                let right = right.parse::<u32>()?;
+                Ok(left..=right)
             };
-            let (first, second) = (split_into_range(first), split_into_range(second));
-            u32::from(first.overlaps(second))
+            let first = split_into_range(first)?;
+            let second = split_into_range(second)?;
+
+            Ok(u32::from(first.overlaps(second)))
         })
-        .sum();
+        .sum::<Result<u32, _>>()?;
 
     println!("Part two: {part_two}");
 
