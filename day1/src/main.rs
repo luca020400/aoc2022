81598436a052582c6d2b4948c1733d0aa0a52f59
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-12-04 17:09:28 +0100

    day1: Propagate error to main

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,21 +3,20 @@
 use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut data: Vec<u32> = fs::read_to_string("input.txt")?
+    let mut data = fs::read_to_string("input.txt")?
         .split("\n\n")
-        .map(|elf| {
-            elf.lines()
-                .map(|calories| calories.parse::<u32>().unwrap())
-                .sum()
-        })
-        .collect();
+        .map(|elf| elf.lines().map(|calories| calories.parse::<u32>()).sum())
+        .collect::<Result<Vec<u32>, _>>()?;
 
     data.sort_unstable();
 
-    let part_one = data.last().unwrap();
-    let part_two = data[data.len() - 3..].iter().sum::<u32>();
-
+    let part_one = match data.last() {
+        Some(x) => Ok(x),
+        None => Err("Must have at least one element"),
+    }?;
     println!("Part one: {part_one}");
+
+    let part_two = data[data.len() - 3..].iter().sum::<u32>();
     println!("Path two: {part_two}");
 
     Ok(())
