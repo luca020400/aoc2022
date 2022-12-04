05405675f2534e0214b10ac0293f6858d76275ed
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-12-04 17:25:14 +0100

    day2: Propagate errors to main

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,10 +9,13 @@ const LOSS: u8 = 0;
 fn main() -> Result<(), Box<dyn Error>> {
     let part_one: u32 = fs::read_to_string("input.txt")?
         .lines()
-        .map(|line| {
-            let (a, b) = line.split_once(' ').unwrap();
-            let mut a = a.parse::<char>().unwrap() as u8;
-            let mut x = b.parse::<char>().unwrap() as u8;
+        .map(|line| -> Result<u32, Box<dyn Error>> {
+            let (a, b) = match line.split_once(' ') {
+                Some(x) => Ok(x),
+                None => Err("Can't split the string"),
+            }?;
+            let mut a = a.parse::<char>()? as u8;
+            let mut x = b.parse::<char>()? as u8;
 
             // Map moves to scores
             a -= b'A';
@@ -20,31 +23,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             // Count score of 2nd player
             // Ensuring we don't overflow
-            u32::from(
+            Ok(u32::from(
                 x + 1
                     + match (x + 3 - a).rem_euclid(3) {
                         1 => WIN,
                         0 => DRAW,
                         _ => LOSS,
                     },
-            )
+            ))
         })
-        .sum();
+        .sum::<Result<u32, _>>()?;
 
     println!("Part one: {part_one}");
 
     let part_two: u32 = fs::read_to_string("input.txt")?
         .lines()
-        .map(|line| {
-            let (a, b) = line.split_once(' ').unwrap();
-            let mut a = a.parse::<char>().unwrap() as u8;
-            let x = b.parse::<char>().unwrap() as u8;
+        .map(|line| -> Result<u32, Box<dyn Error>> {
+            let (a, b) = match line.split_once(' ') {
+                Some(x) => Ok(x),
+                None => Err("Can't split the string"),
+            }?;
+            let mut a = a.parse::<char>()? as u8;
+            let x = b.parse::<char>()? as u8;
 
             // Map move to score
             a -= b'A';
 
             // Ensure we don't overflow
-            u32::from(match x {
+            Ok(u32::from(match x {
                 // Loss
                 b'X' => (a + 3 - 1).rem_euclid(3) + 1 + LOSS,
                 // Draw
@@ -52,9 +58,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 // Win
                 b'Z' => (a + 1).rem_euclid(3) + 1 + WIN,
                 _ => panic!("Unknown win situation"),
-            })
+            }))
         })
-        .sum();
+        .sum::<Result<u32, _>>()?;
 
     println!("Part two: {part_two}");
 
