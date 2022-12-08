f1efe6be900e5440aaad8c812bb0330db374b4fe
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-12-08 16:08:19 +0100

    day8: Rename variables to make sense

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -7,23 +7,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let grid: Vec<&str> = input.lines().collect();
     let mut part_one = 0;
 
-    grid.iter().enumerate().for_each(|(ext_i, row)| {
-        row.bytes().enumerate().for_each(|(ext_j, tree)| {
+    grid.iter().enumerate().for_each(|(i, row)| {
+        row.bytes().enumerate().for_each(|(j, tree)| {
             let mut left = 1;
-            for j in 0..ext_j {
-                left &= u32::from(row.bytes().nth(j).unwrap() < tree);
+            for jj in 0..j {
+                left &= u32::from(row.bytes().nth(jj).unwrap() < tree);
             }
             let mut right = 1;
-            for j in ext_j + 1..row.len() {
-                right &= u32::from(row.bytes().nth(j).unwrap() < tree);
+            for jj in j + 1..row.len() {
+                right &= u32::from(row.bytes().nth(jj).unwrap() < tree);
             }
             let mut up = 1;
-            for i in 0..ext_i {
-                up &= u32::from(grid[i].bytes().nth(ext_j).unwrap() < tree);
+            for ii in 0..i {
+                up &= u32::from(grid[ii].bytes().nth(j).unwrap() < tree);
             }
             let mut down = 1;
-            for i in ext_i + 1..grid.len() {
-                down &= u32::from(grid[i].bytes().nth(ext_j).unwrap() < tree);
+            for ii in i + 1..grid.len() {
+                down &= u32::from(grid[ii].bytes().nth(j).unwrap() < tree);
             }
             part_one += left | right | up | down;
         });
@@ -33,35 +33,35 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut part_two_scores: Vec<u32> = Vec::new();
 
-    grid.iter().enumerate().for_each(|(ext_i, row)| {
-        row.bytes().enumerate().for_each(|(ext_j, tree)| {
+    grid.iter().enumerate().for_each(|(i, row)| {
+        row.bytes().enumerate().for_each(|(j, tree)| {
             let mut left = 0;
-            for j in (0..ext_j).into_iter().rev() {
-                let tree_cmp = row.bytes().nth(j).unwrap();
+            for jj in (0..j).into_iter().rev() {
+                let tree_cmp = row.bytes().nth(jj).unwrap();
                 left = left + 1;
                 if tree_cmp >= tree {
                     break;
                 }
             }
             let mut right = 0;
-            for j in ext_j + 1..row.len() {
-                let tree_cmp = row.bytes().nth(j).unwrap();
+            for jj in j + 1..row.len() {
+                let tree_cmp = row.bytes().nth(jj).unwrap();
                 right = right + 1;
                 if tree_cmp >= tree {
                     break;
                 }
             }
             let mut up = 0;
-            for i in (0..ext_i).into_iter().rev() {
-                let tree_cmp = grid[i].bytes().nth(ext_j).unwrap();
+            for ii in (0..i).into_iter().rev() {
+                let tree_cmp = grid[ii].bytes().nth(j).unwrap();
                 up = up + 1;
                 if tree_cmp >= tree {
                     break;
                 }
             }
             let mut down = 0;
-            for i in ext_i + 1..grid.len() {
-                let tree_cmp = grid[i].bytes().nth(ext_j).unwrap();
+            for ii in i + 1..grid.len() {
+                let tree_cmp = grid[ii].bytes().nth(j).unwrap();
                 down = down + 1;
                 if tree_cmp >= tree {
                     break;
